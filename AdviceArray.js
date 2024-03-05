3eda7263b04bafe3f7d477c3c076764d1edea7de
Reality <ighoafokereality1@gmail.com>
2024-03-05 12:43:45 +0100

    initial commit

---
+++ AdviceArray.js
@@ -0,0 +1,32 @@
+adviceData = [
+  {
+    para: "Focusing on mastering a specific stack is more beneficial than having average proficiency across multiple stacks.Specializatin often leads to deeper expertise and more valuable contributions in a chosen area",
+  },
+  {
+    para: "Consistent practice leads to perfection",
+  },
+  {
+    para: "What distinguisg you from other developers is the additional time you invest",
+  },
+  {
+    para: "It's totaly fine to apply for jobs even before mastering all required language",
+  },
+  {
+    para: "Embrace mistake; they are integral to the learning experience",
+  },
+  {
+    para: "Unpaid experience is beneficial, paid experience is better; Both have value.Avoid internship that offer neither compensation nor valuable experience",
+  },
+  {
+    para: "Build project yeah thats is it build project",
+  },
+  {
+    para: "code with a relax mind for a relax mind absorb information faster",
+  },
+  {
+    para: "Dont run to AI for all challenge your face try to improve on your problem solving ability",
+  },
+  {
+    para: "its perfectly okay to have imposter syndrom at the begining of your career it will fade away as time goes on",
+  },
+];
